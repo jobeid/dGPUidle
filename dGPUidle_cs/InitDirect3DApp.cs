6b0d2d863eb2fe501d453c2834cd0425a8dacd59
jobeid <jobeid@users.noreply.github.com>
2017-03-01 19:14:25 -0500

    Add files via upload

---
+++ dGPUidle_cs/InitDirect3DApp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using SharpDX;
+using SharpDX.Direct3D12;
+using SharpDX.DXGI;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DX12GameProgramming
+{
+    public class InitDirect3DApp : D3DApp
+    {
+        public InitDirect3DApp(IntPtr hInstance) : base(hInstance)
+        {
+            ClientWidth = 300;
+            ClientHeight = 200;
+            MainWindowCaption = "dGPU Idle";
+        }
+        protected override void Update(GameTimer gt)
+        {
+            //long f = CommandQueue.TimestampFrequency;
+            //SetWindowText("Hi, freq=" + f); 
+            //SetWindowText("Hi, Time=" + gt.DeltaTime);
+            for (int i = 0; i < 20; i++)
+            {
+                Application.DoEvents();
+                Thread.Sleep(100);
+            }
+            //string x = "Hi! " + gt.TotalTime;
+            //Debug.WriteLine(x);
+        }
+        protected override void Draw(GameTimer gt)
+        {
+            // Reuse the memory associated with command recording.
+            // We can only reset when the associated command lists have finished execution on the GPU.
+            DirectCmdListAlloc.Reset();
+        }
+        
+        protected override void DoWhilePaused(GameTimer gt)
+        {
+            //string x = "t=" + gt.TotalTime + " et=" + gt.DeltaTime;
+            //Debug.WriteLine(x);
+            // call only if needed: see if you can detect when amber light is red (dGPU off).
+            if(gt.DeltaTime>3.5 || gt.DeltaTime < 0)
+            {
+                //Debug.WriteLine("Calling OnResize()");
+                OnResize();
+            }
+            // wait 3 sec's:
+            for (int i = 0; i < 10; i++)
+            {
+                Application.DoEvents();
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
---
+++ dGPUidle_cs/Program.cs
@@ -0,0 +1,125 @@
+﻿
+namespace DX12GameProgramming
+{
+    using System;
+    using System.Diagnostics;
+    using System.Windows.Forms;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+
+    internal class Program
+    {
+        /// <summary>Enumeration of the different ways of showing a window using
+        /// ShowWindow</summary>
+        private enum WindowShowStyle : uint
+        {
+            /// <summary>Hides the window and activates another window.</summary>
+            /// <remarks>See SW_HIDE</remarks>
+            Hide = 0,
+            /// <summary>Activates and displays a window. If the window is minimized
+            /// or maximized, the system restores it to its original size and
+            /// position. An application should specify this flag when displaying
+            /// the window for the first time.</summary>
+            /// <remarks>See SW_SHOWNORMAL</remarks>
+            ShowNormal = 1,
+            /// <summary>Activates the window and displays it as a minimized window.</summary>
+            /// <remarks>See SW_SHOWMINIMIZED</remarks>
+            ShowMinimized = 2,
+            /// <summary>Activates the window and displays it as a maximized window.</summary>
+            /// <remarks>See SW_SHOWMAXIMIZED</remarks>
+            ShowMaximized = 3,
+            /// <summary>Maximizes the specified window.</summary>
+            /// <remarks>See SW_MAXIMIZE</remarks>
+            Maximize = 3,
+            /// <summary>Displays a window in its most recent size and position.
+            /// This value is similar to "ShowNormal", except the window is not
+            /// actived.</summary>
+            /// <remarks>See SW_SHOWNOACTIVATE</remarks>
+            ShowNormalNoActivate = 4,
+            /// <summary>Activates the window and displays it in its current size
+            /// and position.</summary>
+            /// <remarks>See SW_SHOW</remarks>
+            Show = 5,
+            /// <summary>Minimizes the specified window and activates the next
+            /// top-level window in the Z order.</summary>
+            /// <remarks>See SW_MINIMIZE</remarks>
+            Minimize = 6,
+            /// <summary>Displays the window as a minimized window. This value is
+            /// similar to "ShowMinimized", except the window is not activated.</summary>
+            /// <remarks>See SW_SHOWMINNOACTIVE</remarks>
+            ShowMinNoActivate = 7,
+            /// <summary>Displays the window in its current size and position. This
+            /// value is similar to "Show", except the window is not activated.</summary>
+            /// <remarks>See SW_SHOWNA</remarks>
+            ShowNoActivate = 8,
+            /// <summary>Activates and displays the window. If the window is
+            /// minimized or maximized, the system restores it to its original size
+            /// and position. An application should specify this flag when restoring
+            /// a minimized window.</summary>
+            /// <remarks>See SW_RESTORE</remarks>
+            Restore = 9,
+            /// <summary>Sets the show state based on the SW_ value specified in the
+            /// STARTUPINFO structure passed to the CreateProcess function by the
+            /// program that started the application.</summary>
+            /// <remarks>See SW_SHOWDEFAULT</remarks>
+            ShowDefault = 10,
+            /// <summary>Windows 2000/XP: Minimizes a window, even if the thread
+            /// that owns the window is hung. This flag should only be used when
+            /// minimizing windows from a different thread.</summary>
+            /// <remarks>See SW_FORCEMINIMIZE</remarks>
+            ForceMinimized = 11
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, WindowShowStyle nCmdShow);
+        public static Process PriorProcess()
+        // Returns a System.Diagnostics.Process pointing to
+        // a pre-existing process with the same name as the
+        // current one, if any; or null if the current process
+        // is unique.
+        {
+            Process curr = Process.GetCurrentProcess();
+            Process[] procs = Process.GetProcessesByName(curr.ProcessName);
+            foreach (Process p in procs)
+            {
+                if ((p.Id != curr.Id) &&
+                    (p.MainModule.FileName == curr.MainModule.FileName))
+                {
+                    //IntPtr hWind = p.MainWindowHandle;
+                    //ShowWindow(hWind, WindowShowStyle.ShowNormal);
+                    //MessageBox.Show("hWind = " + hWind);
+                    //Thread.Sleep(100);
+                    //ShowWindow(hWind, WindowShowStyle.ShowNormal);
+                    return p;
+                }
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            Process p = PriorProcess();
+            if (p != null)
+            {
+                DialogResult result = MessageBox.Show("Another instance of the app is already running. \nStart a new instance and continue running?",
+                    "dGPU idle",
+                    MessageBoxButtons.YesNoCancel);
+                if (result != DialogResult.Yes)
+                {
+                    if (result == DialogResult.No) p.Kill();
+                    return;
+                }
+                p.Kill();
+                Application.DoEvents();
+                Thread.Sleep(50);
+            }
+            using (D3DApp app = new InitDirect3DApp(Process.GetCurrentProcess().Handle))
+            {
+                app.InitializeNoFPS(); //JO: don't show FPS
+                //IntPtr hWind = app.GetWindowHandle();
+                //ShowWindow(hWind, WindowShowStyle.Hide);
+                app.WHide();
+                app.Run();
+            }
+        }
+    }
+}
